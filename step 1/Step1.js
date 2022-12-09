8f6cf78e5897044658b1789799fc78c8391d4b3e
raoul-baddawi <raoulbaddawi1@gmail.com>
2022-12-09 02:32:51 +0200

    created clean

--- step 1/Step1.js
+++
@@ -1,7 +0,0 @@
-function borderRed(){
-    document.getElementById("image1").style.border="3px solid red";
-}
-
-function borderBlack(){
-    document.getElementById("image1").style.border="3px solid black";
-}
