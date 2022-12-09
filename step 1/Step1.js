0aff29658dedcd32ba09fa2ebaf258c6bd8c99ef
raoul-baddawi <raoulbaddawi1@gmail.com>
2022-12-09 02:28:36 +0200

    Step1 done

---
+++ step 1/Step1.js
@@ -0,0 +1,7 @@
+function borderRed(){
+    document.getElementById("image1").style.border="3px solid red";
+}
+
+function borderBlack(){
+    document.getElementById("image1").style.border="3px solid black";
+}
