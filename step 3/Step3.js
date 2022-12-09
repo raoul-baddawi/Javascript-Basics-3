fb6ce01d8f8909676d4b15764974ec56c3ff3428
raoul-baddawi <raoulbaddawi1@gmail.com>
2022-12-09 03:31:24 +0200

    Step3 done

---
+++ step 3/Step3.js
@@ -0,0 +1,11 @@
+function red() {
+    document.getElementById("text").style.color="red";
+}
+
+function green() {
+    document.getElementById("text").style.color="green";
+}
+
+function blue() {
+    document.getElementById("text").style.color="lightblue";
+}
